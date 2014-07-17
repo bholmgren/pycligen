97db9f0f45daf6b6536f9cb0c760cfa562d4f9fa
Benny Holmgren <benny@holmgren.id.au>
2014-07-17 16:18:06 +1000

    update copyright header

--- pycligen_cv.h
+++ pycligen_cv.h
@@ -1,23 +1,24 @@
-/*
-  CVS Version: $Id: cligen_cv.h,v 1.3 2013/04/12 10:59:52 olof Exp $ 
-
-  Copyright (C) 2001-2013 Olof Hagsand
-
-  This file is part of CLIgen.
-
-  CLIgen is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  CLIgen is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with CLIgen; see the file COPYING.
-*/
+/* 
+ * pycligen_cv.h
+ *
+ * Copyright (C) 2014 Benny Holmgren
+ *
+ * This file is part of PyCLIgen.
+ *
+ * PyCLIgen is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  PyCLIgen is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along wth PyCLIgen; see the file LICENSE.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef _PY_CLIGEN_CV_H_
 #define _PY_CLIGEN_CV_H_
--- pycligen_pt.h
+++ pycligen_pt.h
@@ -1,23 +1,24 @@
-/*
-  CVS Version: $Id: cligen_cv.h,v 1.3 2013/04/12 10:59:52 olof Exp $ 
-
-  Copyright (C) 2014 Benny Holmgren
-
-  This file is part of CLIgen.
-
-  CLIgen is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  CLIgen is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with CLIgen; see the file COPYING.
-*/
+/* 
+ * pycligen_pt.h
+ *
+ * Copyright (C) 2014 Benny Holmgren
+ *
+ * This file is part of PyCLIgen.
+ *
+ * PyCLIgen is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  PyCLIgen is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along wth PyCLIgen; see the file LICENSE.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef _PY_CLIGEN_PT_H_
 #define _PY_CLIGEN_PT_H_
@@ -26,4 +27,4 @@ extern PyTypeObject ParseTree_Type;
 
 parse_tree *ParseTree_pt(PyObject *pto);
 
-#endif /* _PY_CLIGEN_CV_H_ */
+#endif /* _PY_CLIGEN_PT_H_ */
