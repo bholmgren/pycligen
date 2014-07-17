ae95ce5dd8a37cb5e1c7668bc51f040da4b3676a
Benny Holmgren <benny@holmgren.id.au>
2014-07-17 16:12:35 +1000

    copyright header

--- hello.py
+++ hello.py
@@ -1,6 +1,27 @@
+#!/usr/bin/python
+#
+#  pycligen hello world
+#
+# Copyright (C) 2014 Benny Holmgren
+#
+#  This file is part of PyCLIgen.
+#
+#  PyPyCLIgen is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  PyCLIgen is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with PyCLIgen; see the file LICENSE.
+
 from cligen import *
 
-def cb(cligen, vr, arg):
+def cb(cgen, vr, arg):
     print("cvec = ",vr)
     print("arg = ",arg)
     return 0
