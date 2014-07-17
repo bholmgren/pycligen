fac85b416d3531bf643c6d27434eb63fd0b9e4b0
Benny Holmgren <benny@holmgren.id.au>
2014-07-17 16:18:21 +1000

    update header

--- pycligen.h
+++ pycligen.h
@@ -1,3 +1,25 @@
+/* 
+ * pycligen.h
+ *
+ * Copyright (C) 2014 Benny Holmgren
+ *
+ * This file is part of PyCLIgen.
+ *
+ * PyCLIgen is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  PyCLIgen is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along wth PyCLIgen; see the file LICENSE.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __PY_CLIGEN_H__
 #define __PY_CLIGEN_H__
 
@@ -13,8 +35,7 @@ typedef struct {
 typedef struct _CLIgen {
     PyObject_HEAD
     CLIgen_handle handle;
-    PyObject *globals;
-    parse_tree pt;
+    PyObject *ptlist;	/* List of ParseTrees added */
 } CLIgen;
 
 cg_fnstype_t *CLIgen_str2fn(char *name, void *arg, char **error);
