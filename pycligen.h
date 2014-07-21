806f9e5d18bb895e8c8f40830f984663a28eb0e9
Benny Holmgren <benny@holmgren.id.au>
2014-07-21 15:26:50 +1000

    python2 back port

--- pycligen.h
+++ pycligen.h
@@ -23,9 +23,40 @@
 #ifndef __PY_CLIGEN_H__
 #define __PY_CLIGEN_H__
 
+/*
+ * Support Python 2 and 3
+ */
+#if PY_MAJOR_VERSION >= 3
+  #define MOD_ERROR_VAL NULL
+  #define MOD_SUCCESS_VAL(val) val
+  #define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
+  #define MOD_DEF(ob, name, doc, methods) \
+          static struct PyModuleDef moduledef = { \
+            PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
+          ob = PyModule_Create(&moduledef);
+#else
+  #define MOD_ERROR_VAL
+  #define MOD_SUCCESS_VAL(val)
+  #define MOD_INIT(name) void init##name(void)
+  #define MOD_DEF(ob, name, doc, methods) \
+          ob = Py_InitModule3(name, methods, doc);
+
+  #define PyExc_FileNotFoundError PyExc_IOError
+  #define PyExc_PermissionError PyExc_IOError
+
+#endif
+
+
+cligen_handle CLIgen_cligen_handle(PyObject *cgen);
+
+/* Python 3/2 support functions */
+PyObject *StringFromString(const char *str);
+char *StringAsString(PyObject *obj);
+char *ErrString(int restore);
+
+/* CLIgen callbacks */
 cg_fnstype_t *CLIgen_str2fn(char *name, void *arg, char **error);
 expand_cb *CLIgen_expand_str2fn(char *name, void *arg, char **error);
 
-extern cligen_handle CLIgen_cligen_handle(PyObject *cgen);
 
 #endif /* __PY_CLIGEN_H__ */
