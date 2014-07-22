a3ead28775afd0428659958c7559d707d2d075d6
Benny Holmgren <benny@holmgren.id.au>
2014-07-22 10:57:48 +1000

    use CLIgen.output() instead of print()

--- cligen_tutorial.py
+++ cligen_tutorial.py
@@ -28,26 +28,26 @@ from cligen import *
 def hello(cgen, vars, arg):
     'This callback just prints the function argument'
 
-    print (arg)
+    cgen.output(sys.stdout, "{:s}\n".format(str(arg)))
     return 0
     
 
 def cb(cgen, vars, arg):
     'This is a generic callback printing the variable vector and argument'
 
-    print ('variables:');
+    cgen.output(sys.stdout, 'variables:\n');
     for i, cv in enumerate(vars):
         name = cv.name_get()
         if name is None:
             name = "(null)"
-        print ('\t{:d} name:{:s} type:{:s} value:{:s}'.format(
+        cgen.output(sys.stdout, '\t{:d} name:{:s} type:{:s} value:{:s}\n'.format(
                 i, 
                 name,
                 cv.type2str(),
                 str(cv)))
     
     if arg is not None:
-        print ('\targument: {:s}'.format(str(arg)))
+        cgen.output(sys.stdout, '\targument: {:s}\n'.format(str(arg)))
 
     return 0
 
@@ -56,23 +56,23 @@ def letters(cgen, vars, arg):
     'An example of a callback handling a complex syntax'
 
     if vars.haskey('ca'):
-        print (vars['ca'])
+        cgen.output(sys.stdout, "{:s}\n".format(str(vars['ca'])))
     if vars.haskey('int'):
-        print (vars['int'])
+        cgen.output(sys.stdout, "{:s}\n".format(str(vars['int'])))
     if vars.haskey('cb'):
-        print (vars['cb'])
+        cgen.output(sys.stdout, "{:s}\n".format(str(vars['cb'])))
     if vars.haskey('dd'):
-        print (vars['dd'])
+        cgen.output(sys.stdout, "{:s}\n".format(str(vars['dd'])))
     if vars.haskey('ee'):
-        print (vars['ee'])
+        cgen.output(sys.stdout, "{:s}\n".format(str(vars['ee'])))
 
-
-
+    return 0
+        
 
 def secret(cgen, vars, arg):
     'This callback changes the prompt to the variable setting'
 
-    print ('This is a hidden command: {:s}'.format(str(arg)))
+    cgen.output(sys.stdout, 'This is a hidden command: {:s}\n'.format(str(arg)))
     return 0;
 
 
@@ -83,7 +83,8 @@ def setprompt(cgen, vars, arg):
     
     if vars.haskey('new'):
         cgen.prompt_set(str(vars['new']))
-        return 0;
+
+    return 0;
 
 
 
@@ -97,7 +98,8 @@ def Quit(cgen, vars, arg):
 def unknown(cgen, vars, arg):
     'Command without assigned callback'
     
-    print ('The command has no assigned callback: {:d}'.format(vars[0]))
+    cgen.output(sys.stdout,
+                'The command has no assigned callback: {:d}\n'.format(vars[0]))
     return 0;
 
 
@@ -109,7 +111,7 @@ def interface(cgen, func, cvec, arg):
 
 
 def usage(argv):
-    print ('Usage: {:s} [-h][-f <filename>][-s <syntax>]'.format(sys.argv[0]))
+    cgen.output(sys.stdout, 'Usage: {:s} [-h][-f <filename>][-s <syntax>]\n'.format(sys.argv[0]))
     sys.exit(2)
 
     
@@ -153,8 +155,8 @@ def main(argv):
     c.tree_add(treename, pt)
     c.tree_active_set(treename)
 
-    print ('Syntax:')
-    print(pt)
+    c.output(sys.stdout, 'Syntax:\n')
+    c.output(sys.stdout, pt)
 
     c.eval()
 
