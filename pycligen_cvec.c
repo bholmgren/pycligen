8e65fa4d30e89bba20a4e40767860b0b7c2c8e0b
Benny Holmgren <benny@holmgren.id.au>
2014-08-06 15:13:11 +1000

    implement _cligen.Cvec class add Cvec.items() support

--- pycligen_cvec.c
+++ pycligen_cvec.c
@@ -27,53 +27,131 @@
 #include "pycligen.h"
 #include "pycligen_cv.h"
 
+typedef struct {
+    PyObject_HEAD
+} Cvec;
+
+
+static void
+Cvec_dealloc(Cvec* self)
+{
+    Py_TYPE(self)->tp_free((PyObject*)self);
+}
+
+static PyObject *
+Cvec_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+    return (PyObject *)type->tp_alloc(type, 0);
+}
+
+static int
+Cvec_init(Cvec *self, PyObject *args, PyObject *kwds)
+{
+    return 0;
+}
+
 
 /*
  * Create CLIgen.Cvec instance from cvec
  */
 PyObject *
-Cvec_from_cvec(PyObject *self, cvec *vr)
+__Cvec_from_cvec(PyObject *self, PyObject *args)
 {
-    PyObject *Name = NULL;
-    PyObject *Cls = NULL;
-    PyObject *Cvec = NULL;
+    PyObject *Capsule = NULL;
     PyObject *Cv = NULL;
-    PyObject *Retval = NULL;
-    cg_var *new = NULL;
+    PyObject *Method = NULL;
+    PyObject *Ret;
+    cvec *vr = NULL;
     cg_var *cv;
- 
-    /* Find class object */
-    
-    Name = StringFromString("cligen");
-    if (Name == NULL)
-	return NULL;
-    Cls = PyImport_Import(Name);
-    Py_DECREF(Name);
-    if (Cls == NULL)
-	return NULL;
-    Cvec = PyObject_CallMethod(Cls, "Cvec", NULL);
-    Py_DECREF(Cls);
-    if (Cvec == NULL)
-	return NULL;
-    
-    /* Add CgVars */
-    for (cv = NULL; (cv = cvec_each(vr, cv)); ) {
-	Cv = PyObject_CallMethod(Cvec, "append", "i", cv_type_get(cv));
-	if (Cv == NULL)
-	    goto quit;
-	Py_DECREF(Cv);  /* leave only reference owned by Cvec object */
-	if ((new = CgVar_cv(Cv)) == NULL)
-	    goto quit;
-	if ((cv_cp(new, cv)) < 0) {
-	    PyErr_SetString(PyExc_MemoryError, NULL);
-	    goto quit;
-	}
-    }
-    
-    Retval = Cvec;
-quit:
-    if (Retval == NULL)
-	Py_XDECREF(Cvec);
 
-    return Retval;
+    if (!PyArg_ParseTuple(args, "O", &Capsule))
+        return NULL;
+
+    if ((vr = (cvec *)PyCapsule_GetPointer(Capsule, NULL)) == NULL)
+	return NULL;
+
+    /* Add CgVars */
+    for (cv = NULL; (cv = cvec_each(vr, cv)); ) {	
+	if ((Cv = CgVar_Instance(cv)) == NULL)
+	    return NULL;
+	
+	if ((Method = StringFromString("append")) == NULL)
+	    return NULL;
+	
+	Ret = PyObject_CallMethodObjArgs(self, Method, Cv, NULL);
+	Py_DECREF(Method);
+	Py_DECREF(Cv);  /* leave only reference owned by Cvec object */
+	if (Ret == NULL)
+	    return NULL;
+	Py_DECREF(Ret);
+    }
+
+    Py_RETURN_NONE;
+}
+
+
+
+static PyMethodDef Cvec_methods[] = {
+    
+    {"__Cvec_from_cvec", (PyCFunction)__Cvec_from_cvec, METH_VARARGS, 
+     "Populate self from cvec* pointer"
+    },
+
+
+   {NULL}  /* Sentinel */
+};
+
+PyTypeObject Cvec_Type = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "_cligen.Cvec",            /* tp_name */
+    sizeof(Cvec),              /* tp_basicsize */
+    0,                         /* tp_itemsize */
+    (destructor)Cvec_dealloc,  /* tp_dealloc */
+    0,                         /* tp_print */
+    0,                         /* tp_getattr */
+    0,                         /* tp_setattr */
+    0,                         /* tp_reserved */
+    0,                         /* tp_repr */
+    0,                         /* tp_as_number */
+    0,                         /* tp_as_sequence */
+    0,                         /* tp_as_mapping */
+    0,                         /* tp_hash  */
+    0,                         /* tp_call */
+    0,                         /* tp_str */
+    0,                         /* tp_getattro */
+    0,                         /* tp_setattro */
+    0,                         /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,   /* tp_flags */
+    "Cvec objects",            /* tp_doc */
+    0,                         /* tp_traverse */
+    0,                         /* tp_clear */
+    0,                         /* tp_richcompare */
+    0,                         /* tp_weaklistoffset */
+    0,                         /* tp_iter */
+    0,                         /* tp_iternext */
+    Cvec_methods,              /* tp_methods */
+    0,                         /* tp_members */
+    0,                         /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)Cvec_init,       /* tp_init */
+    0,                         /* tp_alloc */
+    Cvec_new,                  /* tp_new */
+};
+
+int
+Cvec_init_object(PyObject *m)
+{
+
+    if (PyType_Ready(&Cvec_Type) < 0)
+        return -1;
+
+    Py_INCREF(&Cvec_Type);
+    PyModule_AddObject(m, "Cvec", (PyObject *)&Cvec_Type);
+
+    return 0;
 }
